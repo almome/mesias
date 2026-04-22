0f03a900161ab15d830773fcfba53f79d125ba46
almome <alexandra.mome@gmail.com>
2026-04-22 19:08:18 +0200

    corregidos vientos nagoya

--- java-cep/src/main/java/org/example/EsperQueries.java
+++ java-cep/src/main/java/org/example/EsperQueries.java
@@ -132,7 +132,7 @@ public class EsperQueries {
                     "'ALERTA: AIRE CONTAMINADO HACIA CIUDAD' as mensaje " +
                     "FROM pattern [ " +
                     "every e = eNose(airQuality < 30) " +
-                    "-> v = Viento(direction in ('N', 'NE', 'NW', 'NNE', 'NNW', 'W') " +
+                    "-> v = Viento(direction in ('N', 'NE', 'NNE', 'NNW') " +
                     "AND velocidad > 15.0) " +         // <-- corregido: velocidad no velocity
                     "where timer:within(10 min) " +
                     "]";
